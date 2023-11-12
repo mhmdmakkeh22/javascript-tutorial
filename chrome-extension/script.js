71a225a31758c1afa85f282c61cfbe7cedd3a960
Mhmd Makkeh <coretwo10@gmail.com>
2023-11-12 16:23:22 +0200

    chrome extension start

---
+++ chrome-extension/script.js
@@ -0,0 +1,9 @@
+let inputBtn = document.getElementById('input-btn');
+let myLeads = [];
+let inputEl = document.getElementById('input-el');
+
+
+inputBtn.addEventListener('click',function(){
+console.log('Button CLicked');
+});
+
