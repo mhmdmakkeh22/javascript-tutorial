a9df57b55f9210a32758dd8266ed87a73da0dbf9
Mhmd Makkeh <coretwo10@gmail.com>
2023-11-17 12:46:56 +0200

    init

--- chrome-extension/script.js
+++ chrome-extension/script.js
@@ -1,9 +1,27 @@
 let inputBtn = document.getElementById('input-btn');
 let myLeads = [];
 let inputEl = document.getElementById('input-el');
+const ulEl = document.getElementById('ul-el');
 
-
+let lead ;
 inputBtn.addEventListener('click',function(){
-console.log('Button CLicked');
+lead = inputEl.value;
+myLeads.push(lead);
+renderLeads();
 });
-
+function renderLeads(){
+let listItems = "";
+for(let i=0;i<myLeads.length;i++){
+  //listItems +=  '<li> <a href=" '+ myLeads[i]+ '" target="_blanc">'+ myLeads[i] +'</a>' +" " +'</li>';
+  listItems += `<li> 
+                <a href=" ${myLeads[i]} "  target="_blanc">${myLeads[i]}</a>  
+              </li>`;
+    // add an li element to a ul: first way  ulEl.innerHTML +=  '<li>'+ myLeads[i] + " " +'</li>';
+  //second way
+ // const li=document.createElement('li');
+  //li.textContent += myLeads[i] + " ";
+  //ulEl.append(li);
+}
+ulEl.innerHTML = listItems;
+inputEl.value= "";
+}
